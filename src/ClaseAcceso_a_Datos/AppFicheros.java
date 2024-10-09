e7ceae8f15e6a10d2c6b4646508cfadd396dd250
brayan8abo <brayan8abo@outlook.es>
2024-10-09 20:41:42 +0200

    04/10/2024

--- src/ClaseAcceso_a_Datos/AppFicheros.java
+++ src/ClaseAcceso_a_Datos/AppFicheros.java
@@ -1,84 +1,154 @@
-package ClaseAcceso_a_Datos;
+pacimport java.io.*;
+import ClaseAcceso_a_Datos.FileManager;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.Date;
-import java.sql.SQLOutput;
-import java.text.SimpleDateFormat;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AppFicheros {
 
     public static void main(String[] args) {
-        File fichero = new File("fichero_ej.txt");
-        if (!fichero.exists()) {
-            System.out.println("EL FICHERO NO EXISTE");
-            try {
-                //si el fichero no existe, lo creamos
-                fichero.createNewFile();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
 
-        // CREACION DEL DIRECTORIO
-        File directorio = new File("DIRECTORIO_PRUEBA");
-        if (!directorio.exists()) {
-            System.out.println("CREANDO DIR " + directorio.getAbsolutePath());
-            if (!directorio.mkdir()) {
-                System.err.println("FALLO LA CREACION DEL DIRECTORIO " + directorio.getAbsolutePath());
-            } else {
-                System.out.println("\t DIRECTORIO CREADO");
-            }
-        } else { // YA EXISTE
-            // para ver la hora y fecha en la que se modifico
+        FileManager fileManager = new FileManager();
 
-            long time_stamp = directorio.lastModified();
-            Date date = new Date(time_stamp);
-            String pattern = "MM-dd-yyyy HH:mm:ss";
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-            System.out.println("El directorio fue modificado por última vez: " + simpleDateFormat.format(date));
-            //System.out.println("El directorio fue modificado por última vez: " + date);
-        }
-
-        //CONSULTO LA RUTA ABSOLUTA
-        try {
-            String canPath = fichero.getCanonicalPath();
-            String path = fichero.getCanonicalPath();
-
-            System.out.println("\t===== RUTAS DEL FICHERO =====");
-            System.out.println(canPath + "\n" + path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        //LISTAR EL CONTENIDO DE UN DIRECTORIO
-        File fich = new File("C:\\Users\\ALUMNO CCC - TARDE\\IdeaProjects\\Acceso_Datos");
-
-
-//            File[] directorios = fich.listFiles();
-//            for (File f : directorios) {
-//                System.out.println("directorio: " + f.getName());
-//            }
-
-        // optra forma de hacerlo
-//        String [] nombres = fich.list();
-//        for (String nombre : nombres){
-//            System.out.println("directorio: " + nombre);  // split te devuelte un String de cadenas
-//        }
-
-        FileManager fm = new FileManager();
-
-        System.out.println("INICIO COPIA DE ARCHIVO");
         String nombreArchivo = "el_quijote.txt";
+
         String nombreCopia = "el_quijote_copia.txt";
-        if (fm.copiaFicheros(nombreArchivo, nombreCopia)) {
-            File f = new File("BrayanCopia1.txt", "Stiven.txt");
-            System.out.println("Archivo copiado a: " + f.getAbsolutePath());
+
+
+        if (fileManager.copiaFicheros(nombreArchivo, nombreCopia)) {
+            File f = new File(nombreCopia);
+
+            System.out.println("El fichero ha sido copiado en la ruta :" + f.getAbsolutePath());
+            System.out.println();
+
         } else {
-            System.out.println("Error al copiar el archivo");
+            System.out.println("El fichero no se ha podido copiar");
         }
-//        fm.copiaFicheros("brayan1.txt", "brayan1.json");
+
+
+        //COPIAR CON EL OTRO METODO DE COPIAR
+
+
+        String copia = fileManager.copiaFicheroInfo(nombreArchivo, nombreCopia);
+
+        if (copia != null) {
+            System.out.println("COPIA REALIZADA CON EXITO CON EL SEGUNDO METODO DE COPIAR");
+            System.out.println("El fichero ha sido copiado en la ruta :" + copia);
+        } else {
+            System.out.println("El fichero " + nombreArchivo + " no se ha podido copiar");
+        }
+
+        //COPIAR PARCIAL
+
+        if (fileManager.copiaParcialFichero(nombreArchivo, nombreCopia)) {
+
+            System.out.println("El fichero ha sido copiado COrrectamente");
+        }
+
+
     }
 
+    /**
+     * Copia un fichero
+     *
+     * @param rutaOrigen  Ruta del fichero origen
+     * @param rutaDestino Ruta del fichero_destino
+     * @return <true> si se ha copiado, <false> si no
+     */
+    public boolean copiaFichero(String rutaOrigen, String rutaDestino) {
 
+        File ficheroOrigen = new File(rutaOrigen);
+        File ficheroDestino = new File(rutaDestino);
+
+        try {
+            FileInputStream fis = new FileInputStream(ficheroOrigen);
+            FileOutputStream fos = new FileOutputStream(ficheroDestino);
+
+            //  byte[] bytesLeidos = fis.readAllBytes();
+            byte[] bytesLeidos = null;
+
+            do {
+                bytesLeidos = fis.readNBytes(1024);
+                if (bytesLeidos.length > 0) {
+                    fos.write(bytesLeidos);
+                }
+
+            } while (bytesLeidos.length > 0);
+
+            fos.write(bytesLeidos);
+            fis.close();
+            fos.close();
+
+
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+
+        } catch (IOException e) {
+            Logger.getLogger(FileManager.class.getName()).log(Level.SEVERE, null, e);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Copia el fichero de la rutaOrigen a la rutaDestino indicada
+     *
+     * @param rutaOrigen
+     * @param rutaDestino
+     * @ return <String> ruta del fichero copiado
+     */
+    public String copiaFicheroInfo(String rutaOrigen, String rutaDestino) {
+        File f = new File(rutaDestino);
+
+        if (this.copiaFichero(rutaOrigen, rutaDestino)) {
+            return f.getAbsolutePath();
+
+
+        }
+        return null;
+    }
+
+    public boolean copiaParcialFichero(String rutaOrigen, String rutaDestino) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = new FileInputStream(rutaOrigen);
+            out = new FileOutputStream(rutaDestino);
+            byte[] buf = new byte[1024];
+
+            int numBytesLeidos = in.read(buf, 40, 500);
+            System.out.println("SE HAN LEIDO " + numBytesLeidos + " BYTES DEL FICHERO");
+            System.out.printf("SE HAN LEIDO " + numBytesLeidos + " BYTES DEL FICHERO");
+            out.write(buf, 40, numBytesLeidos);
+
+            // numBytesLeidos = in.readNBytes(buf,541,500);
+            //System.out.println("SE HAN LEIDO "+numBytesLeidos+" BYTES DEL FICHERO");
+            //out.write(buf);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
 }
