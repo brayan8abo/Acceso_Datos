2933c6e07c0ccdf10dc56aa64c8491a715d40f58
brayan8abo <brayan8abo@outlook.es>
2024-10-10 19:48:40 +0200

    04/10/2024

---
+++ src/ClaseAcceso_a_Datos/FileManagerCaracteres.java
@@ -0,0 +1,31 @@
+package ClaseAcceso_a_Datos;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileManagerCaracteres {
+
+    public static void escribeMensajeFichero(String rutaFichero, String contenido) {
+
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(rutaFichero);
+            fw.write(contenido);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (fw != null) {
+                    fw.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
---
+++ src/ClaseAcceso_a_Datos/Main.java
@@ -0,0 +1,43 @@
+package ClaseAcceso_a_Datos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main{
+    public static void main(String[] args) {
+
+        File fichero = new File("fichero_ej.txt");
+
+        if(!fichero.exists()){
+            System.out.println("EL FICHERO NO EXISTEE");
+            try {
+                fichero.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        File directorio = new File("directorio_prueba");
+        if (!directorio.exists()){
+
+            if (!directorio.mkdir()){
+                System.err.println("fallo la creacion del directorio");
+            }
+            else {
+                System.out.println("\tDirectorio creado");
+            }
+
+        }
+        else {
+            //YA EXISTE
+
+            long time_stamp = directorio.lastModified();
+            Date date = new Date(time_stamp);
+
+        }
+
+
+    }
+}
