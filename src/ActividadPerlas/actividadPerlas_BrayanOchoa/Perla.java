7a5c76df257fa690869e50f17921f7b7a479eec1
Brayan8abo <brayan8abo@outlook.es>
2024-11-28 00:00:20 +0100

    Actividad perlas

---
+++ src/ActividadPerlas/actividadPerlas_BrayanOchoa/Perla.java
@@ -0,0 +1,14 @@
+package ActividadPerlas;
+
+public class Perla {
+	private final String color;
+
+	public Perla(String color) {
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+}
+
